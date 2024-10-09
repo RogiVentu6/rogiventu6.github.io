2d2495e58b93b8cfcb67f4bfd6bfb86617203e7a
RogiVentu6 <rogerventura96@gmail.com>
2024-10-09 23:01:54 +0200

    more stuff

--- src/components/Evaluaciones.tsx
+++ src/components/Evaluaciones.tsx
@@ -7,6 +7,20 @@ const Evaluaciones: React.FC = () => {
       <div className="hero-content">
         <h2>Evaluaciones</h2>
         <p>Aquí va la información relevante sobre evaluaciones.</p>
+        <p>
+          La psicología es el arte de entender y acompañar tu viaje emocional. En este espacio, encontrarás un 
+          refugio donde puedes ser tú mismo y explorar tus sentimientos. 
+        </p>
+        <p>
+          Cada pensamiento es una puerta que se abre hacia el entendimiento. La salud emocional no es solo la 
+          ausencia de problemas, sino la presencia de bienestar. Aquí, juntos, trabajaremos para que 
+          encuentres la paz y la claridad que mereces.
+        </p>
+        <img 
+          src={require('../assets/images/psicoIA3.png')} // Ajusta la extensión del archivo si es necesario
+          alt="Psicología y bienestar"
+          className="main-image"
+        />
       </div>
     </section>
   );
--- src/components/Otros.tsx
+++ src/components/Otros.tsx
@@ -7,6 +7,20 @@ const Otros: React.FC = () => {
       <div className="hero-content">
         <h2>Otros</h2>
         <p>Aquí va la información relevante sobre otros.</p>
+        <p>
+          La psicología es el arte de entender y acompañar tu viaje emocional. En este espacio, encontrarás un 
+          refugio donde puedes ser tú mismo y explorar tus sentimientos. 
+        </p>
+        <p>
+          Cada pensamiento es una puerta que se abre hacia el entendimiento. La salud emocional no es solo la 
+          ausencia de problemas, sino la presencia de bienestar. Aquí, juntos, trabajaremos para que 
+          encuentres la paz y la claridad que mereces.
+        </p>
+        <img 
+          src={require('../assets/images/psicoIA4.png')} // Ajusta la extensión del archivo si es necesario
+          alt="Psicología y bienestar"
+          className="main-image"
+        />
       </div>
     </section>
   );
