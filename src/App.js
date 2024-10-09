1942f1b96c00e8f3933c8771cca19e285bf72ca1
RogiVentu6 <rogerventura96@gmail.com>
2024-10-09 19:27:32 +0200

    first approach

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Hello world this is my static web in github pages.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
